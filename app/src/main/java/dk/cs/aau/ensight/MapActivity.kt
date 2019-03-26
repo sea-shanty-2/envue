5126aaea313eef91fd356e9effe0671f2a4386b5
Theis Jendal <jendal@live.dk>
2019-03-26 14:25:15 +0100

    Added heatmap with earthquake as temporary data

--- app/src/main/java/dk/cs/aau/ensight/MapActivity.kt
+++ app/src/main/java/dk/cs/aau/ensight/MapActivity.kt
@@ -8,9 +8,29 @@ import com.mapbox.mapboxsdk.annotations.Marker
 import com.mapbox.mapboxsdk.annotations.MarkerOptions
 import com.mapbox.mapboxsdk.geometry.LatLng
 import com.mapbox.mapboxsdk.maps.*
+import com.mapbox.mapboxsdk.style.expressions.Expression
+import com.mapbox.mapboxsdk.style.layers.HeatmapLayer
 import dk.cs.aau.ensight.Utils.textToBitmap
+import com.mapbox.mapboxsdk.style.layers.CircleLayer
+import com.mapbox.mapboxsdk.style.expressions.Expression.*
+import com.mapbox.mapboxsdk.style.layers.PropertyFactory.*
+import com.mapbox.mapboxsdk.style.sources.GeoJsonSource
+import java.net.MalformedURLException
+import java.net.URL
 
-class MapActivity : AppCompatActivity(), OnMapReadyCallback, MapboxMap.OnMarkerClickListener {
+
+class MapActivity : AppCompatActivity(), OnMapReadyCallback, MapboxMap.OnMarkerClickListener, Style.OnStyleLoaded{
+    private val EARTHQUAKE_SOURCE_URL = "https://www.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson"
+    private val EARTHQUAKE_SOURCE_ID = "earthquakes"
+    private val HEATMAP_LAYER_ID = "earthquakes-heat"
+    private val HEATMAP_LAYER_SOURCE = "earthquakes"
+    private val CIRCLE_LAYER_ID = "earthquakes-circle"
+
+    override fun onStyleLoaded(style: Style) {
+        addEarthquakeSource(style)
+        addHeatmapLayer(style)
+    }
+
     private var mMap: MapboxMap? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,7 +51,7 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback, MapboxMap.OnMarkerC
 
     override fun onMapReady(mapboxMap: MapboxMap) {
         mMap = mapboxMap
-        mMap?.setStyle(Style.MAPBOX_STREETS)
+        mMap?.setStyle(Style.MAPBOX_STREETS, this)
         addMarker(LatLng(50.0, 50.0), "\uD83D\uDE1A", 50)
         mMap?.setOnMarkerClickListener(this)
     }
@@ -45,6 +65,127 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback, MapboxMap.OnMarkerC
         var descriptor = IconFactory.getInstance(this).fromBitmap(bitmap)
         mMap?.addMarker(MarkerOptions().position(position).icon(descriptor))
         return
+    }
+
+    private fun addEarthquakeSource(loadedMapStyle: Style) {
+        try {
+            var tmp = GeoJsonSource(EARTHQUAKE_SOURCE_ID, URL(EARTHQUAKE_SOURCE_URL))
+            loadedMapStyle.addSource(tmp)
+        } catch (malformedUrlException: MalformedURLException) {
+            print("My heart")
+        }
+
+    }
+
+    private fun addHeatmapLayer(loadedMapStyle: Style) {
+        val layer = HeatmapLayer(HEATMAP_LAYER_ID, EARTHQUAKE_SOURCE_ID)
+        layer.maxZoom = 19f
+        layer.setSourceLayer(HEATMAP_LAYER_SOURCE)
+        layer.setProperties(
+
+            // Color ramp for heatmap.  Domain is 0 (low) to 1 (high).
+            // Begin color ramp at 0-stop with a 0-transparency color
+            // to create a blur-like effect.
+            heatmapColor(
+                interpolate(
+                    linear(), heatmapDensity(),
+                    literal(0), rgba(33, 102, 172, 0),
+                    literal(0.2), rgb(103, 169, 207),
+                    literal(0.4), rgb(209, 229, 240),
+                    literal(0.6), rgb(253, 219, 199),
+                    literal(0.8), rgb(239, 138, 98),
+                    literal(1), rgb(178, 24, 43)
+                )
+            ),
+
+            // Increase the heatmap weight based on frequency and property magnitude
+            heatmapWeight(
+                interpolate(
+                    linear(), get("mag"),
+                    stop(0, 0),
+                    stop(6, 1)
+                )
+            ),
+
+            // Increase the heatmap color weight weight by zoom level
+            // heatmap-intensity is a multiplier on top of heatmap-weight
+            heatmapIntensity(
+                interpolate(
+                    linear(), zoom(),
+                    stop(0, 1),
+                    stop(19, 3)
+                )
+            ),
+
+            // Adjust the heatmap radius by zoom level
+            heatmapRadius(
+                interpolate(
+                    linear(), zoom(),
+                    stop(0, 2),
+                    stop(19, 20)
+                )
+            ),
+
+            // Transition from heatmap to circle layer by zoom level
+            heatmapOpacity(
+                interpolate(
+                    linear(), zoom(),
+                    stop(13, 1),
+                    stop(19, 0)
+                )
+            )
+        )
+
+        loadedMapStyle.addLayerAbove(layer, "waterway-label")
+    }
+
+    private fun addCircleLayer(loadedMapStyle: Style) {
+        val circleLayer = CircleLayer(CIRCLE_LAYER_ID, EARTHQUAKE_SOURCE_ID)
+        circleLayer.setProperties(
+
+            // Size circle radius by earthquake magnitude and zoom level
+            circleRadius(
+                interpolate(
+                    linear(), zoom(),
+                    literal(7), interpolate(
+                        linear(), get("mag"),
+                        stop(1, 1),
+                        stop(6, 4)
+                    ),
+                    literal(16), interpolate(
+                        linear(), get("mag"),
+                        stop(1, 5),
+                        stop(6, 50)
+                    )
+                )
+            ),
+
+            // Color circle by earthquake magnitude
+            circleColor(
+                interpolate(
+                    linear(), get("mag"),
+                    literal(1), rgba(33, 102, 172, 0),
+                    literal(2), rgb(103, 169, 207),
+                    literal(3), rgb(209, 229, 240),
+                    literal(4), rgb(253, 219, 199),
+                    literal(5), rgb(239, 138, 98),
+                    literal(6), rgb(178, 24, 43)
+                )
+            ),
+
+            // Transition from heatmap to circle layer by zoom level
+            circleOpacity(
+                interpolate(
+                    linear(), zoom(),
+                    stop(7, 0),
+                    stop(8, 1)
+                )
+            ),
+            circleStrokeColor("white"),
+            circleStrokeWidth(1.0f)
+        )
+
+        loadedMapStyle.addLayerBelow(circleLayer, HEATMAP_LAYER_ID)
     }
 
 
