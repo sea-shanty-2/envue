5a0737766983b390744b4c689575fe2c324cb375
minibrams <anders@brams.dk>
2019-04-03 02:58:59 +0200

    Trying new emoji support libraries

--- app/src/main/java/dk/cs/aau/envue/InitializeBroadcastActivity.kt
+++ app/src/main/java/dk/cs/aau/envue/InitializeBroadcastActivity.kt
@@ -2,6 +2,8 @@ package dk.cs.aau.envue
 
 import android.os.Bundle
 import android.support.design.widget.Snackbar
+import android.support.text.emoji.EmojiCompat
+import android.support.text.emoji.bundled.BundledEmojiCompatConfig
 import android.support.v7.app.AppCompatActivity;
 
 import kotlinx.android.synthetic.main.activity_initialize_broadcast.*
@@ -11,6 +13,7 @@ class InitializeBroadcastActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_initialize_broadcast)
+        EmojiCompat.init(BundledEmojiCompatConfig(this))
 
         startBroadcastButton.setOnClickListener { view ->
             Snackbar.make(view, "nibber", 2)
