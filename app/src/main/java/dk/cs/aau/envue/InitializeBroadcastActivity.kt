0aff39f934c1a6a3b06211b86d67a6a224cd7aa8
minibrams <anders@brams.dk>
2019-04-04 22:59:09 +0200

    Splitting emoji-string into individual emojis in preparation for category-loading

--- app/src/main/java/dk/cs/aau/envue/InitializeBroadcastActivity.kt
+++ app/src/main/java/dk/cs/aau/envue/InitializeBroadcastActivity.kt
@@ -2,6 +2,7 @@ package dk.cs.aau.envue
 
 import android.content.Intent
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.text.emoji.EmojiCompat
 import android.support.text.emoji.bundled.BundledEmojiCompatConfig
 import android.support.v7.app.AppCompatActivity
@@ -14,8 +15,6 @@ import kotlinx.android.synthetic.main.activity_initialize_broadcast.*
 
 
 class InitializeBroadcastActivity : AppCompatActivity() {
-
-    var hasEmojis: () -> Unit = {}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,6 +37,9 @@ class InitializeBroadcastActivity : AppCompatActivity() {
 
     }
 
+    /** Loads emojis from a JSON file provided by the resource id.
+      * Emojis are loaded directly into a list adapter used by the
+      * broadcast category list view. */
     private fun loadEmojis(resourceId: Int) {
         val allEmojis = GsonBuilder().create().fromJson(
             resources.openRawResource(resourceId).bufferedReader(),
@@ -59,6 +61,7 @@ class InitializeBroadcastActivity : AppCompatActivity() {
         }
     }
 
+    /** Returns all the emojis selected by the user. */
     private fun getSelectedCategories(): List<String> {
         val emojiIcons = (findViewById<ListView>(R.id.broadcastCategoryListView).adapter as BroadcastCategoryListAdapter)
             .getAllEmojis()
@@ -68,6 +71,8 @@ class InitializeBroadcastActivity : AppCompatActivity() {
         return emojiIcons.map {it.char}
     }
 
+    /** Starts the broadcast after processing selected categories
+      * if all checks pass. */
     private fun startBroadcast(view: View) {
         if (!isOnlyEmojis(emojiSearchField.text.toString())) {
             Snackbar.make(
@@ -76,10 +81,57 @@ class InitializeBroadcastActivity : AppCompatActivity() {
         }
 
         val selectedEmojis = getSelectedCategories()  // TODO: Do something with this
-        startActivity(Intent(this, BroadcastActivity::class.java))
+        Snackbar.make(view, " and ".join(emojiStringToArray(emojiSearchField.text.toString())), Snackbar.LENGTH_LONG).show()
+        //startActivity(Intent(this, BroadcastActivity::class.java))
     }
 
+    /** Returns true if the string consists of only emojis or is empty, otherwise false. */
     private fun isOnlyEmojis(str: String): Boolean {  // LMFAO REGEX
-        return EmojiCompat.get().hasEmojiGlyph(str)
+        return str.matches(Regex("(?:[\uD83C\uDF00-\uD83D\uDDFF]|[\uD83E\uDD00-\uD83E\uDDFF]|" +
+                "[\uD83D\uDE00-\uD83D\uDE4F]|[\uD83D\uDE80-\uD83D\uDEFF]|" +
+                "[\u2600-\u26FF]\uFE0F?|[\u2700-\u27BF]\uFE0F?|\u24C2\uFE0F?|" +
+                "[\uD83C\uDDE6-\uD83C\uDDFF]{1,2}|" +
+                "[\uD83C\uDD70\uD83C\uDD71\uD83C\uDD7E\uD83C\uDD7F\uD83C\uDD8E\uD83C\uDD91-\uD83C\uDD9A]\uFE0F?|" +
+                "[\u0023\u002A\u0030-\u0039]\uFE0F?\u20E3|[\u2194-\u2199\u21A9-\u21AA]\uFE0F?|[\u2B05-\u2B07\u2B1B\u2B1C\u2B50\u2B55]\uFE0F?|" +
+                "[\u2934\u2935]\uFE0F?|[\u3030\u303D]\uFE0F?|[\u3297\u3299]\uFE0F?|" +
+                "[\uD83C\uDE01\uD83C\uDE02\uD83C\uDE1A\uD83C\uDE2F\uD83C\uDE32-\uD83C\uDE3A\uD83C\uDE50\uD83C\uDE51]\uFE0F?|" +
+                "[\u203C\u2049]\uFE0F?|[\u25AA\u25AB\u25B6\u25C0\u25FB-\u25FE]\uFE0F?|" +
+                "[\u00A9\u00AE]\uFE0F?|[\u2122\u2139]\uFE0F?|\uD83C\uDC04\uFE0F?|\uD83C\uDCCF\uFE0F?|" +
+                "[\u231A\u231B\u2328\u23CF\u23E9-\u23F3\u23F8-\u23FA]\uFE0F?)+")) || str.isEmpty()
+    }
+
+    /** Separates a string of (only!) emojis into an array list of individual emojis. */
+    private fun emojiStringToArray(emojiString: String): ArrayList<String> {
+        val arr = ArrayList<String>()
+        var accumulator = ""
+        for (i in 0 until emojiString.length) {
+            if (accumulator.isNotEmpty()) {
+                accumulator += emojiString[i]
+                arr.add(accumulator)
+                accumulator = ""
+            } else {
+                accumulator = emojiString[i].toString()
+                if (isOnlyEmojis(accumulator)) {
+                    arr.add(accumulator)
+                    accumulator = ""
+                }
+            }
+        }
+
+        return arr
+    }
+
+    /** Returns a string of elements in the provided list joined by the origin string. */
+    fun <T> String.join(other: Iterable<T>): String {
+        var joined = ""
+        other as Collection<T>
+        if (other.size < 2) {
+            return if (other.isNotEmpty()) other.first().toString() else joined
+        }
+        for (element in other) {
+            joined += "${element.toString()}${this}"
+        }
+
+        return joined.removeSuffix(this)
     }
 }
