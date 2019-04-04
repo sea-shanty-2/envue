25a883c2e527eaf2d4832a9cc56131ee07f483e4
minibrams <anders@brams.dk>
2019-04-04 21:43:01 +0200

    Added an emojis-only check to the emoji-search-field

--- app/src/main/java/dk/cs/aau/envue/InitializeBroadcastActivity.kt
+++ app/src/main/java/dk/cs/aau/envue/InitializeBroadcastActivity.kt
@@ -9,6 +9,7 @@ import android.support.text.emoji.EmojiCompat
 import android.support.text.emoji.bundled.BundledEmojiCompatConfig
 import android.support.text.emoji.widget.EmojiTextView
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import android.widget.LinearLayout
 import android.widget.ListView
 import android.widget.TextView
@@ -26,8 +27,7 @@ class InitializeBroadcastActivity : AppCompatActivity() {
         EmojiCompat.init(BundledEmojiCompatConfig(this))
 
         startBroadcastButton.setOnClickListener { view ->
-            val selectedEmojis = getSelectedCategories()  // TODO: Do something with this
-            startActivity(Intent(this, BroadcastActivity::class.java))
+            startBroadcast(view)
         }
 
         emojiSearchField.setOnFocusChangeListener { view, hasFocus ->
@@ -68,8 +68,33 @@ class InitializeBroadcastActivity : AppCompatActivity() {
             .filter {it.isSelected}
             .map {it.getEmoji()}
 
-        return emojiIcons.map {
-            it.char
+        return emojiIcons.map {it.char}
+    }
+
+    private fun startBroadcast(view: View) {
+        if (!isOnlyEmojis(emojiSearchField.text.toString())) {
+            Snackbar.make(
+                view, resources.getString(R.string.illegal_char_in_search_for_emoji_field), Snackbar.LENGTH_LONG).show()
+            return
         }
+
+        val selectedEmojis = getSelectedCategories()  // TODO: Do something with this
+        startActivity(Intent(this, BroadcastActivity::class.java))
+    }
+
+    private fun isOnlyEmojis(str: String): Boolean {  // LMFAO REGEX
+        return str.matches(Regex("(?:[\uD83C\uDF00-\uD83D\uDDFF]|[\uD83E\uDD00-\uD83E\uDDFF]|" +
+                "[\uD83D\uDE00-\uD83D\uDE4F]|[\uD83D\uDE80-\uD83D\uDEFF]|" +
+                "[\u2600-\u26FF]\uFE0F?|[\u2700-\u27BF]\uFE0F?|\u24C2\uFE0F?|" +
+                "[\uD83C\uDDE6-\uD83C\uDDFF]{1,2}|" +
+                "[\uD83C\uDD70\uD83C\uDD71\uD83C\uDD7E\uD83C\uDD7F\uD83C\uDD8E\uD83C\uDD91-\uD83C\uDD9A]\uFE0F?|" +
+                "[\u0023\u002A\u0030-\u0039]\uFE0F?\u20E3|[\u2194-\u2199\u21A9-\u21AA]\uFE0F?|[\u2B05-\u2B07\u2B1B\u2B1C\u2B50\u2B55]\uFE0F?|" +
+                "[\u2934\u2935]\uFE0F?|[\u3030\u303D]\uFE0F?|[\u3297\u3299]\uFE0F?|" +
+                "[\uD83C\uDE01\uD83C\uDE02\uD83C\uDE1A\uD83C\uDE2F\uD83C\uDE32-\uD83C\uDE3A\uD83C\uDE50\uD83C\uDE51]\uFE0F?|" +
+                "[\u203C\u2049]\uFE0F?|[\u25AA\u25AB\u25B6\u25C0\u25FB-\u25FE]\uFE0F?|" +
+                "[\u00A9\u00AE]\uFE0F?|[\u2122\u2139]\uFE0F?|\uD83C\uDC04\uFE0F?|\uD83C\uDCCF\uFE0F?|" +
+                "[\u231A\u231B\u2328\u23CF\u23E9-\u23F3\u23F8-\u23FA]\uFE0F?)+"
+            )
+        )
     }
 }
