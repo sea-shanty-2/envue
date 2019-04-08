17464b1dab31ea60b0742854c9e4e9e3e736b79a
minibrams <anders@brams.dk>
2019-04-08 11:19:02 +0200

    Added unit tests including known emoji-string-split bug

--- app/src/main/java/dk/cs/aau/envue/InitializeBroadcastActivity.kt
+++ app/src/main/java/dk/cs/aau/envue/InitializeBroadcastActivity.kt
@@ -67,6 +67,42 @@ class InitializeBroadcastActivity : AppCompatActivity() {
         }
     }
 
+    /** Returns true if the string consists of only emojis or is empty, otherwise false. */
+    fun isOnlyEmojis(str: String): Boolean {  // LMFAO REGEX
+        return str.matches(Regex("(?:[\uD83C\uDF00-\uD83D\uDDFF]|[\uD83E\uDD00-\uD83E\uDDFF]|" +
+                "[\uD83D\uDE00-\uD83D\uDE4F]|[\uD83D\uDE80-\uD83D\uDEFF]|" +
+                "[\u2600-\u26FF]\uFE0F?|[\u2700-\u27BF]\uFE0F?|\u24C2\uFE0F?|" +
+                "[\uD83C\uDDE6-\uD83C\uDDFF]{1,2}|" +
+                "[\uD83C\uDD70\uD83C\uDD71\uD83C\uDD7E\uD83C\uDD7F\uD83C\uDD8E\uD83C\uDD91-\uD83C\uDD9A]\uFE0F?|" +
+                "[\u0023\u002A\u0030-\u0039]\uFE0F?\u20E3|[\u2194-\u2199\u21A9-\u21AA]\uFE0F?|[\u2B05-\u2B07\u2B1B\u2B1C\u2B50\u2B55]\uFE0F?|" +
+                "[\u2934\u2935]\uFE0F?|[\u3030\u303D]\uFE0F?|[\u3297\u3299]\uFE0F?|" +
+                "[\uD83C\uDE01\uD83C\uDE02\uD83C\uDE1A\uD83C\uDE2F\uD83C\uDE32-\uD83C\uDE3A\uD83C\uDE50\uD83C\uDE51]\uFE0F?|" +
+                "[\u203C\u2049]\uFE0F?|[\u25AA\u25AB\u25B6\u25C0\u25FB-\u25FE]\uFE0F?|" +
+                "[\u00A9\u00AE]\uFE0F?|[\u2122\u2139]\uFE0F?|\uD83C\uDC04\uFE0F?|\uD83C\uDCCF\uFE0F?|" +
+                "[\u231A\u231B\u2328\u23CF\u23E9-\u23F3\u23F8-\u23FA]\uFE0F?)+")) || str.isEmpty()
+    }
+
+    /** Separates a string of (only!) emojis into an array list of individual emojis. */
+    fun emojiStringToArray(emojiString: String): ArrayList<String> {
+        val arr = ArrayList<String>()
+        var accumulator = ""
+        for (i in 0 until emojiString.length) {
+            if (accumulator.isNotEmpty()) {
+                accumulator += emojiString[i]
+                arr.add(accumulator.toString())
+                accumulator = ""
+            } else {
+                accumulator = emojiString[i].toString()
+                if (isOnlyEmojis(accumulator)) {
+                    arr.add(accumulator.toString())
+                    accumulator = ""
+                }
+            }
+        }
+
+        return arr
+    }
+
     /** Returns all the emojis selected by the user.
      * Includes emojis chosen in the grid view as well
      * as emojis searched by the user. */
@@ -104,42 +140,6 @@ class InitializeBroadcastActivity : AppCompatActivity() {
 
         Snackbar.make(view, " and ".join(selectedEmojis), Snackbar.LENGTH_LONG).show()  // For testing
         //startActivity(Intent(this, BroadcastActivity::class.java))
-    }
-
-    /** Returns true if the string consists of only emojis or is empty, otherwise false. */
-    private fun isOnlyEmojis(str: String): Boolean {  // LMFAO REGEX
-        return str.matches(Regex("(?:[\uD83C\uDF00-\uD83D\uDDFF]|[\uD83E\uDD00-\uD83E\uDDFF]|" +
-                "[\uD83D\uDE00-\uD83D\uDE4F]|[\uD83D\uDE80-\uD83D\uDEFF]|" +
-                "[\u2600-\u26FF]\uFE0F?|[\u2700-\u27BF]\uFE0F?|\u24C2\uFE0F?|" +
-                "[\uD83C\uDDE6-\uD83C\uDDFF]{1,2}|" +
-                "[\uD83C\uDD70\uD83C\uDD71\uD83C\uDD7E\uD83C\uDD7F\uD83C\uDD8E\uD83C\uDD91-\uD83C\uDD9A]\uFE0F?|" +
-                "[\u0023\u002A\u0030-\u0039]\uFE0F?\u20E3|[\u2194-\u2199\u21A9-\u21AA]\uFE0F?|[\u2B05-\u2B07\u2B1B\u2B1C\u2B50\u2B55]\uFE0F?|" +
-                "[\u2934\u2935]\uFE0F?|[\u3030\u303D]\uFE0F?|[\u3297\u3299]\uFE0F?|" +
-                "[\uD83C\uDE01\uD83C\uDE02\uD83C\uDE1A\uD83C\uDE2F\uD83C\uDE32-\uD83C\uDE3A\uD83C\uDE50\uD83C\uDE51]\uFE0F?|" +
-                "[\u203C\u2049]\uFE0F?|[\u25AA\u25AB\u25B6\u25C0\u25FB-\u25FE]\uFE0F?|" +
-                "[\u00A9\u00AE]\uFE0F?|[\u2122\u2139]\uFE0F?|\uD83C\uDC04\uFE0F?|\uD83C\uDCCF\uFE0F?|" +
-                "[\u231A\u231B\u2328\u23CF\u23E9-\u23F3\u23F8-\u23FA]\uFE0F?)+")) || str.isEmpty()
-    }
-
-    /** Separates a string of (only!) emojis into an array list of individual emojis. */
-    private fun emojiStringToArray(emojiString: String): ArrayList<String> {
-        val arr = ArrayList<String>()
-        var accumulator = ""
-        for (i in 0 until emojiString.length) {
-            if (accumulator.isNotEmpty()) {
-                accumulator += emojiString[i]
-                arr.add(accumulator.toString())
-                accumulator = ""
-            } else {
-                accumulator = emojiString[i].toString()
-                if (isOnlyEmojis(accumulator)) {
-                    arr.add(accumulator.toString())
-                    accumulator = ""
-                }
-            }
-        }
-
-        return arr
     }
 
     /** Returns a string of elements in the provided list joined by the origin string. */
