bd843ed59578f74a1dc491f624345cf828cea436
BullDuckling <tbuhla@gmail.com>
2019-04-10 12:24:15 +0200

    Profile interests dialog

---
+++ app/src/main/java/dk/cs/aau/envue/InterestsActivity.kt
@@ -0,0 +1,157 @@
+package dk.cs.aau.envue
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.text.emoji.EmojiCompat
+import android.support.text.emoji.bundled.BundledEmojiCompatConfig
+import android.support.v7.app.AppCompatActivity;
+import android.view.View
+import android.widget.EditText
+import android.widget.ListView
+import com.facebook.Profile
+import com.google.gson.GsonBuilder
+import dk.cs.aau.envue.utility.EmojiIcon
+import dk.cs.aau.envue.workers.BroadcastCategoryListAdapter
+
+import kotlinx.android.synthetic.main.activity_interests.*
+
+class InterestsActivity : AppCompatActivity() {
+    companion object {
+        internal val INTERESTS_RESPONSE_KEY = "interests"
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_interests)
+        EmojiCompat.init(BundledEmojiCompatConfig(this))
+
+        setInterestsButton.setOnClickListener { view ->
+            setInterests(view)
+        }
+
+        interestsEmojiSearchField.setOnFocusChangeListener { view, hasFocus ->
+            if (hasFocus) {
+                interestsEmojiSearchField.hint = ""
+            } else {
+                interestsEmojiSearchField.hint = resources.getString(R.string.search_for_emoji_text)
+            }
+        }
+
+        loadEmojis(R.raw.emojis, R.id.interestsCategoryListView)
+
+    }
+
+    /** Loads emojis from a JSON file provided by the resource id.
+     * Emojis are loaded directly into a list adapter used by the
+     * broadcast category list view. */
+    fun loadEmojis(resourceId: Int, targetResourceId: Int) {
+        val allEmojis = GsonBuilder().create().fromJson(
+            resources.openRawResource(resourceId).bufferedReader(),
+            Array<EmojiIcon>::class.java
+        )
+
+        // Wrap all unicodes in EmojiIcon objects in rows of 5
+        var emojiRows = ArrayList<ArrayList<EmojiIcon>>()
+        for (i in 0 until allEmojis.size) {
+            if (i % 5 == 0) {
+                emojiRows.add(ArrayList())
+            }
+            emojiRows.last().add(allEmojis[i])
+        }
+
+        // Provide an item adapter to the ListView
+        findViewById<ListView>(targetResourceId).apply {
+            this.adapter = BroadcastCategoryListAdapter(this.context, emojiRows)
+        }
+    }
+
+    /** Starts the broadcast after processing selected categories
+     * if all checks pass. */
+    private fun setInterests(view: View) {
+        if (!isOnlyEmojis(interestsEmojiSearchField.text.toString())) {
+            Snackbar.make(
+                view, resources.getString(R.string.illegal_char_in_search_for_emoji_field), Snackbar.LENGTH_LONG).show()
+            return
+        }
+
+        val selectedEmojis = getSelectedCategories()  // TODO: Do something with this
+
+        //Snackbar.make(view, " and ".join(selectedEmojis), Snackbar.LENGTH_LONG).show()  // For testing
+
+        val data = Intent().putExtra(INTERESTS_RESPONSE_KEY, " ".join(selectedEmojis))
+        //startActivity(Intent(this, BroadcastActivity::class.java))
+        setResult(Activity.RESULT_OK, data)
+        finish()
+    }
+
+    /** Returns all the emojis selected by the user.
+     * Includes emojis chosen in the grid view as well
+     * as emojis searched by the user. */
+    private fun getSelectedCategories(): List<String> {
+        val gridViewEmojis = (findViewById<ListView>(R.id.interestsCategoryListView).adapter as BroadcastCategoryListAdapter)
+            .getAllEmojis()
+            .filter {it.isSelected}
+            .map {it.getEmoji()}
+
+        val searchedEmojis = (findViewById<EditText>(R.id.interestsEmojiSearchField)).text.toString().run {
+            emojiStringToArray(this)
+        }
+        return if (searchedEmojis.isEmpty()) {
+            gridViewEmojis.map {it.char}
+        } else {
+            gridViewEmojis.map {it.char}.plus(searchedEmojis)
+        }
+    }
+
+    /** Returns true if the string consists of only emojis or is empty, otherwise false. */
+    fun isOnlyEmojis(str: String): Boolean {  // LMFAO REGEX
+        return str.matches(Regex("(?:[\uD83C\uDF00-\uD83D\uDDFF]|[\uD83E\uDD00-\uD83E\uDDFF]|" +
+                "[\uD83D\uDE00-\uD83D\uDE4F]|[\uD83D\uDE80-\uD83D\uDEFF]|" +
+                "[\u2600-\u26FF]\uFE0F?|[\u2700-\u27BF]\uFE0F?|\u24C2\uFE0F?|" +
+                "[\uD83C\uDDE6-\uD83C\uDDFF]{1,2}|" +
+                "[\uD83C\uDD70\uD83C\uDD71\uD83C\uDD7E\uD83C\uDD7F\uD83C\uDD8E\uD83C\uDD91-\uD83C\uDD9A]\uFE0F?|" +
+                "[\u0023\u002A\u0030-\u0039]\uFE0F?\u20E3|[\u2194-\u2199\u21A9-\u21AA]\uFE0F?|[\u2B05-\u2B07\u2B1B\u2B1C\u2B50\u2B55]\uFE0F?|" +
+                "[\u2934\u2935]\uFE0F?|[\u3030\u303D]\uFE0F?|[\u3297\u3299]\uFE0F?|" +
+                "[\uD83C\uDE01\uD83C\uDE02\uD83C\uDE1A\uD83C\uDE2F\uD83C\uDE32-\uD83C\uDE3A\uD83C\uDE50\uD83C\uDE51]\uFE0F?|" +
+                "[\u203C\u2049]\uFE0F?|[\u25AA\u25AB\u25B6\u25C0\u25FB-\u25FE]\uFE0F?|" +
+                "[\u00A9\u00AE]\uFE0F?|[\u2122\u2139]\uFE0F?|\uD83C\uDC04\uFE0F?|\uD83C\uDCCF\uFE0F?|" +
+                "[\u231A\u231B\u2328\u23CF\u23E9-\u23F3\u23F8-\u23FA]\uFE0F?)+")) || str.isEmpty()
+    }
+
+    /** Separates a string of (only!) emojis into an array list of individual emojis. */
+    fun emojiStringToArray(emojiString: String): ArrayList<String> {
+        val arr = ArrayList<String>()
+        var accumulator = ""
+        for (i in 0 until emojiString.length) {
+            if (accumulator.isNotEmpty()) {
+                accumulator += emojiString[i]
+                arr.add(accumulator.toString())
+                accumulator = ""
+            } else {
+                accumulator = emojiString[i].toString()
+                if (isOnlyEmojis(accumulator)) {
+                    arr.add(accumulator.toString())
+                    accumulator = ""
+                }
+            }
+        }
+
+        return arr
+    }
+
+    /** Returns a string of elements in the provided list joined by the origin string. */
+    fun <T> String.join(other: Iterable<T>): String {
+        var joined = ""
+        other as Collection<T>
+        if (other.size < 2) {
+            return if (other.isNotEmpty()) other.first().toString() else joined
+        }
+        for (element in other) {
+            joined += "${element.toString()}${this}"
+        }
+
+        return joined.removeSuffix(this)
+    }
+
+}
--- app/src/main/java/dk/cs/aau/envue/ProfileActivity.kt
+++ app/src/main/java/dk/cs/aau/envue/ProfileActivity.kt
@@ -1,17 +1,40 @@
 package dk.cs.aau.envue
 
+import android.app.Activity
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.Build
 import android.os.Bundle
+import android.support.text.emoji.EmojiCompat
+import android.support.text.emoji.bundled.BundledEmojiCompatConfig
+import android.support.v4.app.NotificationCompat
+import android.support.v4.app.NotificationManagerCompat
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
+import android.widget.ListView
 import com.facebook.Profile
 import com.facebook.login.LoginManager
+import com.google.gson.GsonBuilder
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_profile.*
 import dk.cs.aau.envue.transformers.CircleTransform
+import dk.cs.aau.envue.utility.EmojiIcon
+import dk.cs.aau.envue.workers.BroadcastCategoryListAdapter
+import kotlinx.android.synthetic.main.activity_initialize_broadcast.*
 
 class ProfileActivity : AppCompatActivity() {
+    companion object {
+        internal val SET_INTERESTS_REQUEST = 0
+        internal val INTERESTS_RESPONSE_KEY = "interests"
+        internal val CURRENT_INTERESTS_KEY = "current interests"
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +44,9 @@ class ProfileActivity : AppCompatActivity() {
 
         // register log out button listener
         logOutButton.setOnClickListener { this.onLogOut() }
+        // register change interests button listener
+        interestsButton.setOnClickListener { this.onChangeInterests() }
+        testButton.setOnClickListener { this.onTestNotification() }
 
         val profilePicture = profile.getProfilePictureUri(1024, 1024)
         val profileName = profile.name
@@ -51,6 +77,48 @@ class ProfileActivity : AppCompatActivity() {
             }}
             .create()
             .show()
+    }
+
+    private fun onChangeInterests() {
+        startActivityForResult(Intent(this, InterestsActivity::class.java).putExtra(CURRENT_INTERESTS_KEY, currentInterestsView.text), SET_INTERESTS_REQUEST)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            SET_INTERESTS_REQUEST ->
+                if (resultCode == Activity.RESULT_OK) {
+                    currentInterestsView.text = data?.getStringExtra(INTERESTS_RESPONSE_KEY)
+                }
+        }
+    }
+
+    private fun onTestNotification() {
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val icon: Bitmap = BitmapFactory.decodeResource(this.getResources(), R.drawable.dummy)
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+        var builder = NotificationCompat.Builder(this, getString(R.string.notification_channel_id))
+            .setSmallIcon(R.drawable.ic_live_tv_48dp)
+            .setContentTitle("Notification!!!")
+            .setContentText("This is a notification")
+            .setLargeIcon(icon)
+            .setStyle(NotificationCompat.BigPictureStyle()
+                .bigPicture(icon)
+                .bigLargeIcon(null))
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            // Set the intent that will fire when the user taps the notification
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+
+        with(NotificationManagerCompat.from(this)) {
+            // notificationId is a unique int for each notification that you must define
+            notify(1, builder.build())
+        }
+
+
+
     }
 
 }
