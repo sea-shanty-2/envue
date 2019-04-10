f237ea11b25af80850c15448c9eff36ac780a413
minibrams <anders@brams.dk>
2019-04-10 14:27:46 +0200

    Finalized broadcaster creation and changing to broadcast activity

--- app/src/main/java/dk/cs/aau/envue/InitializeBroadcastActivity.kt
+++ app/src/main/java/dk/cs/aau/envue/InitializeBroadcastActivity.kt
@@ -1,5 +1,6 @@
 package dk.cs.aau.envue
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.text.emoji.EmojiCompat
@@ -7,12 +8,10 @@ import android.support.text.emoji.bundled.BundledEmojiCompatConfig
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.ListView
-import com.apollographql.apollo.ApolloClient
 import com.google.gson.GsonBuilder
 import dk.cs.aau.envue.utility.EmojiIcon
 import dk.cs.aau.envue.workers.BroadcastCategoryListAdapter
 import kotlinx.android.synthetic.main.activity_initialize_broadcast.*
-import okhttp3.OkHttpClient
 import android.util.Log
 import com.apollographql.apollo.ApolloCall
 import com.apollographql.apollo.api.Response
@@ -38,7 +37,6 @@ class InitializeBroadcastActivity : AppCompatActivity() {
         }
 
         loadEmojis(R.raw.emojis, R.id.broadcastCategoryListView)
-        theFunThing()
     }
 
     /** Loads emojis from a JSON file provided by the resource id.
@@ -47,10 +45,10 @@ class InitializeBroadcastActivity : AppCompatActivity() {
     fun loadEmojis(resourceId: Int, targetResourceId: Int) {
 
         // Load all emojis into local storage
-        _allEmojis.plus(GsonBuilder().create().fromJson(
+        _allEmojis = _allEmojis.plus(GsonBuilder().create().fromJson(
             resources.openRawResource(resourceId).bufferedReader(),
             Array<EmojiIcon>::class.java
-        ))
+        )) as ArrayList
 
 
         // Wrap all unicodes in EmojiIcon objects in rows of 5
@@ -66,42 +64,6 @@ class InitializeBroadcastActivity : AppCompatActivity() {
         findViewById<ListView>(targetResourceId).apply {
             this.adapter = BroadcastCategoryListAdapter(this.context, emojiRows)
         }
-    }
-
-    /** Returns true if the string consists of only emojis or is empty, otherwise false. */
-    fun isOnlyEmojis(str: String): Boolean {  // LMFAO REGEX
-        return str.matches(Regex("(?:[\uD83C\uDF00-\uD83D\uDDFF]|[\uD83E\uDD00-\uD83E\uDDFF]|" +
-                "[\uD83D\uDE00-\uD83D\uDE4F]|[\uD83D\uDE80-\uD83D\uDEFF]|" +
-                "[\u2600-\u26FF]\uFE0F?|[\u2700-\u27BF]\uFE0F?|\u24C2\uFE0F?|" +
-                "[\uD83C\uDDE6-\uD83C\uDDFF]{1,2}|" +
-                "[\uD83C\uDD70\uD83C\uDD71\uD83C\uDD7E\uD83C\uDD7F\uD83C\uDD8E\uD83C\uDD91-\uD83C\uDD9A]\uFE0F?|" +
-                "[\u0023\u002A\u0030-\u0039]\uFE0F?\u20E3|[\u2194-\u2199\u21A9-\u21AA]\uFE0F?|[\u2B05-\u2B07\u2B1B\u2B1C\u2B50\u2B55]\uFE0F?|" +
-                "[\u2934\u2935]\uFE0F?|[\u3030\u303D]\uFE0F?|[\u3297\u3299]\uFE0F?|" +
-                "[\uD83C\uDE01\uD83C\uDE02\uD83C\uDE1A\uD83C\uDE2F\uD83C\uDE32-\uD83C\uDE3A\uD83C\uDE50\uD83C\uDE51]\uFE0F?|" +
-                "[\u203C\u2049]\uFE0F?|[\u25AA\u25AB\u25B6\u25C0\u25FB-\u25FE]\uFE0F?|" +
-                "[\u00A9\u00AE]\uFE0F?|[\u2122\u2139]\uFE0F?|\uD83C\uDC04\uFE0F?|\uD83C\uDCCF\uFE0F?|" +
-                "[\u231A\u231B\u2328\u23CF\u23E9-\u23F3\u23F8-\u23FA]\uFE0F?)+")) || str.isEmpty()
-    }
-
-    /** Separates a string of (only!) emojis into an array list of individual emojis. */
-    fun emojiStringToArray(emojiString: String): ArrayList<String> {
-        val arr = ArrayList<String>()
-        var accumulator = ""
-        for (i in 0 until emojiString.length) {
-            if (accumulator.isNotEmpty()) {
-                accumulator += emojiString[i]
-                arr.add(accumulator.toString())
-                accumulator = ""
-            } else {
-                accumulator = emojiString[i].toString()
-                if (isOnlyEmojis(accumulator)) {
-                    arr.add(accumulator.toString())
-                    accumulator = ""
-                }
-            }
-        }
-
-        return arr
     }
 
     /** Returns all the emojis selected by the user.
@@ -127,8 +89,7 @@ class InitializeBroadcastActivity : AppCompatActivity() {
             return
         }
 
-        Snackbar.make(view, " and ".join(selectedEmojiIcons.map { it.char }), Snackbar.LENGTH_LONG).show()  // For testing
-        //startActivity(Intent(this, BroadcastActivity::class.java))
+        startActivity(Intent(this, BroadcastActivity::class.java))
     }
 
     /** Returns a string of elements in the provided list joined by the origin string. */
@@ -145,30 +106,8 @@ class InitializeBroadcastActivity : AppCompatActivity() {
         return joined.removeSuffix(this)
     }
 
-    fun theFunThing() {
-        val baseUrl = resources.getString(R.string.graphql_base_url)
-        val okHttpClient = OkHttpClient.Builder().build()
-        val apolloClient = ApolloClient.builder()
-            .serverUrl(baseUrl)
-            .okHttpClient(okHttpClient)
-            .build()
-
-        val activeQuery = AccountsQuery.builder().first(5).build()
-        val TAG = "InitializeBroadcast"
-        val testCall: ApolloCall<AccountsQuery.Data> = apolloClient.query(activeQuery)
-        var data: AccountsQuery.Data? = null
-        testCall.enqueue(object : ApolloCall.Callback<AccountsQuery.Data>() {
-            override fun onResponse(response: Response<AccountsQuery.Data>){
-                Log.d(TAG, "Did shit")
-                var d = response.data()
-                Log.d(TAG, "SUCCESS: ${d.toString()}")
-            }
-            override fun onFailure(e: ApolloException){
-                Log.d(TAG, e.message)
-            }
-        })
-    }
-
+    /** Creates a broadcaster object and stores it in stable storage
+     * on the Envue database. */
     private fun createBroadcaster(latitude: Double, longitude: Double, category: DoubleArray) {
 
         val location = LocationInputType.builder().latitude(latitude).longitude(longitude).build()
--- app/src/test/java/dk/cs/aau/envue/unitTests/InitializeBroadcastActivityUnitTests.kt
+++ app/src/test/java/dk/cs/aau/envue/unitTests/InitializeBroadcastActivityUnitTests.kt
@@ -11,36 +11,36 @@ class InitializeBroadcastActivityUnitTests {
         Assert.assertEquals(4, 2 + 2)
     }
 
-    @Test
-    fun emojiStringToArray_emptyString_emptyArray() {
-        val str = ""
-        val expected = Array(0) {""}
-        val actual = InitializeBroadcastActivity().emojiStringToArray(str).toArray()
-        Assert.assertArrayEquals(expected, actual)
-    }
-
-    @Test
-    fun emojiStringToArray_simpleEmoji_singletonArray() {
-        val emojiStr = "\uD83D\uDE00"
-        val expected = arrayOf(emojiStr)
-        val actual = InitializeBroadcastActivity().emojiStringToArray(emojiStr).toArray()
-        Assert.assertArrayEquals(expected, actual)
-    }
-
-    @Test
-    fun emojiStringToArray_singleCharEmojis_correctNumberEmojis() {
-        val str = "\uD83D\uDE00\uD83D\uDE01\uD83D\uDE02\uD83E\uDD23\uD83D\uDE03\uD83D\uDE04\uD83D\uDE05\uD83D\uDE06"
-        val expected = arrayOf("😀", "😁","😂", "🤣", "😃", "😄", "😅", "😆")
-        val actual = InitializeBroadcastActivity().emojiStringToArray(str).toArray()
-        Assert.assertArrayEquals(expected, actual)
-    }
-
-    @Test
-    fun emojiStringToArray_singleCharMultipleCharEmojis_correctNumberEmojis() {
-        val str = "\uD83D\uDE00\uD83D\uDE01\uD83D\uDE02\uD83E\uDD23\uD83D\uDE03\uD83D\uDE04\uD83D\uDE05\uD83D\uDE06\uD83D\uDC69\uD83C\uDFFD\u200D\uD83C\uDFA8"
-        val expected = arrayOf("😀", "😁","😂", "🤣", "😃", "😄", "😅", "😆", "👩🏽‍🎨")
-        val actual = InitializeBroadcastActivity().emojiStringToArray(str).toArray()
-        Assert.assertEquals(expected.size, actual.size)
-
-    }
+//    @Test
+//    fun emojiStringToArray_emptyString_emptyArray() {
+//        val str = ""
+//        val expected = Array(0) {""}
+//        val actual = InitializeBroadcastActivity().emojiStringToArray(str).toArray()
+//        Assert.assertArrayEquals(expected, actual)
+//    }
+//
+//    @Test
+//    fun emojiStringToArray_simpleEmoji_singletonArray() {
+//        val emojiStr = "\uD83D\uDE00"
+//        val expected = arrayOf(emojiStr)
+//        val actual = InitializeBroadcastActivity().emojiStringToArray(emojiStr).toArray()
+//        Assert.assertArrayEquals(expected, actual)
+//    }
+//
+//    @Test
+//    fun emojiStringToArray_singleCharEmojis_correctNumberEmojis() {
+//        val str = "\uD83D\uDE00\uD83D\uDE01\uD83D\uDE02\uD83E\uDD23\uD83D\uDE03\uD83D\uDE04\uD83D\uDE05\uD83D\uDE06"
+//        val expected = arrayOf("😀", "😁","😂", "🤣", "😃", "😄", "😅", "😆")
+//        val actual = InitializeBroadcastActivity().emojiStringToArray(str).toArray()
+//        Assert.assertArrayEquals(expected, actual)
+//    }
+//
+//    @Test
+//    fun emojiStringToArray_singleCharMultipleCharEmojis_correctNumberEmojis() {
+//        val str = "\uD83D\uDE00\uD83D\uDE01\uD83D\uDE02\uD83E\uDD23\uD83D\uDE03\uD83D\uDE04\uD83D\uDE05\uD83D\uDE06\uD83D\uDC69\uD83C\uDFFD\u200D\uD83C\uDFA8"
+//        val expected = arrayOf("😀", "😁","😂", "🤣", "😃", "😄", "😅", "😆", "👩🏽‍🎨")
+//        val actual = InitializeBroadcastActivity().emojiStringToArray(str).toArray()
+//        Assert.assertEquals(expected.size, actual.size)
+//
+//    }
 }
