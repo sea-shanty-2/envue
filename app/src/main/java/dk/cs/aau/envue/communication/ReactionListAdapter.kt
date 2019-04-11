ba7f32b7762730c2e110ff128afc05d9bf7d018c
minibrams <anders@brams.dk>
2019-04-11 15:04:10 +0200

    Added reactions to landscape, rotating animations

--- app/src/main/java/dk/cs/aau/envue/communication/ReactionListAdapter.kt
+++ app/src/main/java/dk/cs/aau/envue/communication/ReactionListAdapter.kt
@@ -4,11 +4,21 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import dk.cs.aau.envue.R
+import android.animation.ValueAnimator
+import android.view.animation.AccelerateDecelerateInterpolator
+import android.widget.ImageView
+import kotlinx.android.synthetic.main.fragment_reaction.view.*
+
 
 class ReactionListAdapter(private val onClick: (String) -> Unit, private val emojiList: List<String>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
+
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        holder.itemView.setOnClickListener { onClick(emojiList[position]) }
+        holder.itemView.setOnClickListener {
+            onClick(emojiList[position])
+
+            animateRotation((holder as EmojiImageHolder).itemView.reaction_image)
+        }
 
         return (holder as EmojiImageHolder).bind(emojiList[position])
     }
@@ -19,5 +29,19 @@ class ReactionListAdapter(private val onClick: (String) -> Unit, private val emo
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         return EmojiImageHolder(LayoutInflater.from(parent.context).inflate(R.layout.fragment_reaction, parent,false))
+    }
+
+    private fun animateRotation(image: ImageView) {
+        val rotationAnimator = ValueAnimator.ofFloat(0f, 360f)
+
+        rotationAnimator.duration = 500
+        rotationAnimator.interpolator = AccelerateDecelerateInterpolator()
+
+        rotationAnimator.addUpdateListener { valueAnimator ->
+            val animatedValue = valueAnimator.animatedValue as Float
+            image.rotation = animatedValue
+        }
+
+        rotationAnimator.start()
     }
 }
