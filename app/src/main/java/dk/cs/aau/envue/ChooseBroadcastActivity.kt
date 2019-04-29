90c98769e8ebb85e343806fc3f5ce3021a642e08
minibrams <anders@brams.dk>
2019-04-29 15:42:39 +0200

    Broadcasts listed and ready for formatting

--- app/src/main/java/dk/cs/aau/envue/ChooseBroadcastActivity.kt
+++ app/src/main/java/dk/cs/aau/envue/ChooseBroadcastActivity.kt
@@ -1,16 +1,16 @@
 package dk.cs.aau.envue
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.util.Log
-import android.widget.TextView
+import android.widget.Button
 import com.apollographql.apollo.ApolloCall
 import com.apollographql.apollo.api.Response
 import com.apollographql.apollo.exception.ApolloException
 import dk.cs.aau.envue.shared.GatewayClient
-import dk.cs.aau.envue.utility.ActiveBroadcast
 import dk.cs.aau.envue.workers.ChooseBroadcastListAdapter
 
 class ChooseBroadcastActivity : AppCompatActivity() {
@@ -23,12 +23,20 @@ class ChooseBroadcastActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_choose_broadcast)
         loadBroadcasts()
+
+        findViewById<Button>(R.id.watch_broadcast_btn).setOnClickListener {
+            startActivity(Intent(this, PlayerActivity::class.java))
+        }
     }
 
     private fun loadBroadcasts() {
 
+        // Launch the query
+        // TODO: We should have an EventsQuery that fetches broadcast clusters, not the broadcasts themselves
         val broadcastsQuery = BroadcastsQuery.builder().build()
         GatewayClient.query(broadcastsQuery).enqueue(object: ApolloCall.Callback<BroadcastsQuery.Data>() {
+
+            // Load the broadcasts into the recycler view when received
             override fun onResponse(response: Response<BroadcastsQuery.Data>) {
                 val data = response.data()
                 if (data == null) {
@@ -37,10 +45,10 @@ class ChooseBroadcastActivity : AppCompatActivity() {
 
                 val broadcasts = data?.broadcasts()?.active()?.items()?.toTypedArray()
                 if (broadcasts != null) {
+                    // Populate the recycler view with the broadcasts (should be events)
                     initializeRecyclerView(broadcasts)
                 }
             }
-
 
             override fun onFailure(e: ApolloException) {
                 Log.d("LOAD_FAILURE", e.toString())
@@ -48,6 +56,8 @@ class ChooseBroadcastActivity : AppCompatActivity() {
         })
     }
 
+    /**
+     * Populates the RecyclerView with the broadcasts (should be events, see the to-do) **/
     private fun initializeRecyclerView(items: Array<BroadcastsQuery.Item>) {
         runOnUiThread {
             viewManager = LinearLayoutManager(this)
--- app/src/main/java/dk/cs/aau/envue/workers/ChooseBroadcastListAdapter.kt
+++ app/src/main/java/dk/cs/aau/envue/workers/ChooseBroadcastListAdapter.kt
@@ -1,32 +1,27 @@
 package dk.cs.aau.envue.workers
 
-import android.support.design.widget.Snackbar
 import android.support.v7.widget.RecyclerView
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Button
 import dk.cs.aau.envue.BroadcastsQuery
 import dk.cs.aau.envue.R
 
 class ChooseBroadcastListAdapter(private val items: Array<BroadcastsQuery.Item>) :
     RecyclerView.Adapter<ChooseBroadcastListAdapter.ChooseBroadcastViewHolder>() {
 
-    // Provide a reference to the views for each data item
-    // Complex data items may need more than one view per item, and
-    // you provide access to all the views for a data item in a view holder.
-    // Each data item is just a string in this case that is shown in a TextView.
-    class ChooseBroadcastViewHolder(val activeBroadcastView: TextView) : RecyclerView.ViewHolder(activeBroadcastView)
+    class ChooseBroadcastViewHolder(val activeBroadcastView: Button) : RecyclerView.ViewHolder(activeBroadcastView)
 
     // Create new views (invoked by the layout manager)
     override fun onCreateViewHolder(parent: ViewGroup,
                                     viewType: Int): ChooseBroadcastListAdapter.ChooseBroadcastViewHolder {
-        // create a new view
-        val textView = LayoutInflater.from(parent.context)
-            .inflate(R.layout.active_broadcast_view, parent, false) as TextView
-        // set the view's size, margins, paddings and layout parameters
-        // ...
-        return ChooseBroadcastViewHolder(textView)
+
+        val broadcastButton = LayoutInflater.from(parent.context)
+            .inflate(R.layout.active_broadcast_view, parent, false) as Button
+
+
+        return ChooseBroadcastViewHolder(broadcastButton)
     }
 
     // Replace the contents of a view (invoked by the layout manager)
@@ -34,10 +29,14 @@ class ChooseBroadcastListAdapter(private val items: Array<BroadcastsQuery.Item>)
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
         holder.activeBroadcastView.apply {
-            text = items[position].toString()
-            setOnClickListener {view ->
-                Log.d("Click", "You clicked broadcast ${items[position].id()}!")
+            val broadcast = items[position]
+
+            setOnClickListener {
+                Log.d("Click", "You clicked broadcast ${broadcast.id()}!")
             }
+
+            text = "${broadcast.id()}\n Lat(${broadcast.location()?.latitude()}), Lon(${broadcast.location()?.longitude()})"
+
         }
     }
 
