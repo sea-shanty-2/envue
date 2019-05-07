1f45efc84a92adc74d758426a39068cd51685632
minibrams <anders@brams.dk>
2019-05-07 16:57:41 +0200

    First draft of statistics doen

--- app/src/main/java/dk/cs/aau/envue/StatisticsActivity.kt
+++ app/src/main/java/dk/cs/aau/envue/StatisticsActivity.kt
@@ -2,10 +2,13 @@ package dk.cs.aau.envue
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.util.Log
 import android.view.View
 import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.charts.LineChart
 import com.github.mikephil.charting.data.*
+import kotlinx.android.synthetic.main.activity_statistics.*
 import kotlin.random.Random
 
 
@@ -18,59 +21,81 @@ class StatisticsActivity : AppCompatActivity() {
         val keys = intent.extras?.keySet()
         val extrasFromKeys = keys?.map { k -> intent.extras?.get(k) }
 
-        val joined = intent.extras?.get("joinedTimestamps") as Array<Int>
-        val left = intent.extras?.get("leftTimestamps") as Array<Int>
+        val joined = (intent.extras?.get("joinedTimestamps") as Array<Int>).apply {sort()}
+        val left = (intent.extras?.get("leftTimestamps") as Array<Int>).apply {sort()}
 
+        val chart = findViewById<View>(R.id.chart) as LineChart
 
-        val chart = findViewById<View>(R.id.chart) as BarChart
+        if (!joined.isEmpty() || !left.isEmpty()) {
+            val dataSet = LineDataSet(generateChartData(
+                joined.map { i -> i - joined.min()!! }.toTypedArray(),
+                left.map { i -> i - joined.min()!! }.toTypedArray()),
+                "Viewer numbers")
 
-        val dataSet = BarDataSet(generateChartData(joined, left), "The data")
-        chart.setDrawGridBackground(false)
-        chart.data = BarData(dataSet)
-        chart.invalidate()  // Refresh
-
+            chart.setDrawGridBackground(false)
+            chart.data = LineData(dataSet.apply {
+                setDrawFilled(true)
+                setDrawCircleHole(false)
+                setDrawCircles(false)
+                lineWidth = 0f
+                fillDrawable = ContextCompat.getDrawable(this@StatisticsActivity, R.drawable.fade_green)
+            })
+            chart.invalidate()  // Refresh
+        }
     }
 
-    private fun generateChartData(u: Array<Int>, v: Array<Int>): MutableList<BarEntry> {
-        val chartData = ArrayList<Int>()
-        var i=0; var j=0; var numCurrentViewers=0
+    private fun generateChartData(u: Array<Int>, v: Array<Int>): MutableList<Entry> {
+        // Generate placeholders for y
 
-        // Increment the num viewers when finding a smaller joined
-        // timestamp (from vector u) and decrement in left time stamps
-        // (vector v).
-        while (i < u.size && j < v.size) {
-            when {
-                u[i] < v[j] -> {
-                    numCurrentViewers++; i++
-                    chartData.add(numCurrentViewers)
-                }
-                u[i] > v[j] -> {
-                    numCurrentViewers--; j++
-                    chartData.add(numCurrentViewers)
-                }
-                else -> {
+        fun merge(u: Array<Int>, v: Array<Int>): ArrayList<Pair<Int, Boolean>> {
+            var i = 0
+            var j = 0
+            val merged = ArrayList<Pair<Int, Boolean>>()
+
+            while (i < u.size && j < v.size) {
+                if (u[i] < v[j]) {
+                    merged.add(Pair(u[i], true))
+                    i++
+                } else if (u[i] > v[j]) {
+                    merged.add(Pair(v[j], false))
+                    j++
+                } else {
+                    merged.add(Pair(u[i], true))
+                    merged.add(Pair(v[j], false))
                     i++; j++
-                    chartData.add(numCurrentViewers)
                 }
             }
+
+            while (i < u.size) {
+                merged.add(Pair(u[i], true))
+                i++
+            }
+
+            while (j < v.size) {
+                merged.add(Pair(v[j], false))
+                j++
+            }
+
+            return merged
         }
 
-        // Now either u or v is done, so process the rest of the lists
-        while (i < u.size) {
-            numCurrentViewers++; i++
-            chartData.add(numCurrentViewers)
+        val merged = merge(u, v)
+        val xs = (0 until Math.max(u.max()!!, v.max()!!))
+        val ys = ArrayList<Int>()
+
+        var numCurrentViewers = 0
+        for (pair in merged) {
+            while (ys.size < pair.first) {
+                ys.add(numCurrentViewers)
+            }
+            numCurrentViewers += if (pair.second) 1 else -1
         }
 
-        while (j < v.size) {
-            numCurrentViewers--; j++
-            chartData.add(numCurrentViewers)
+        val barChartData = ArrayList<Entry>()
+        for (pair in xs.zip(ys)) {
+            barChartData.add(Entry(1f * pair.first, 1f * pair.second))
         }
 
-        Log.d("BROADIDTIMES", "Merged: $chartData")
-
-        // Convert to BarChart entries
-        val barChartEntries = (0 until chartData.size).map { i -> BarEntry(i * 1f, chartData[i] * 1f) }
-
-        return barChartEntries.toMutableList()
+        return barChartData.toMutableList()
     }
 }
