2b4221b7877894e79946d84e67dd8c6543234df4
Rógvi Eliasen <seragath@live.com>
2019-05-09 11:25:50 +0200

    Removed a few method calls to HandshakePacket that were redundant.

--- app/src/main/java/dk/cs/aau/envue/communication/StreamCommunicationListener.kt
+++ app/src/main/java/dk/cs/aau/envue/communication/StreamCommunicationListener.kt
@@ -16,7 +16,6 @@ import okio.ByteString
 class StreamCommunicationListener(private val communicationListener: CommunicationListener,
                                   private val channelId: String) : WebSocketListener() {
     override fun onOpen(webSocket: WebSocket, response: Response) {
-        communicationListener.onConnected()
 
         val profileQuery = ProfileQuery.builder().build()
         var displayName = ""
@@ -28,29 +27,24 @@ class StreamCommunicationListener(private val communicationListener: Communicati
                     Log.e("DisplayName","$displayName + Profile not null")
                     displayName = profile.displayName()
 
-                    webSocket.send(Gson().toJson(
-                        HandshakePacket(
-                            displayName,
-                            Profile.getCurrentProfile().getProfilePictureUri(256, 256).toString(),
-                            channelId
-                        )
-                    ))
-
                 }
                 else {
-                    webSocket.send(Gson().toJson(
-                        HandshakePacket(
-                            "Anon",//Profile.getCurrentProfile().name,
-                            Profile.getCurrentProfile().getProfilePictureUri(256, 256).toString(),
-                            channelId
-                        )
-                    ))
+                    displayName = "FFS"
                 }
             }
 
             override fun onFailure(e: ApolloException){}
 
         })
+        communicationListener.onConnected()
+
+        webSocket.send(Gson().toJson(
+            HandshakePacket(
+                displayName,
+                Profile.getCurrentProfile().getProfilePictureUri(256, 256).toString(),
+                channelId
+            )
+        ))
 
     }
 
