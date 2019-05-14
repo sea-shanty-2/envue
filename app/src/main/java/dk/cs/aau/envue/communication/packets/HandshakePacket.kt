3db85cde92206bfb966762a6837ed65c14942734
Anders Jakobsen <andersjkbsn@gmail.com>
2019-05-14 15:05:17 +0200

    Send unique id

--- app/src/main/java/dk/cs/aau/envue/communication/packets/HandshakePacket.kt
+++ app/src/main/java/dk/cs/aau/envue/communication/packets/HandshakePacket.kt
@@ -1,5 +1,5 @@
 package dk.cs.aau.envue.communication.packets
 
-class HandshakePacket(val Name: String, val Channel: String) {
+class HandshakePacket(val Name: String, val Channel: String, val UniqueId: String) {
     val Type = "Identity"
 }
