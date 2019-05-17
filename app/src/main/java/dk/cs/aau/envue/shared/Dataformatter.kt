a953f62dbfad398789c2b3ee2cd1833f02cdabbe
Theis Jendal <jendal@live.dk>
2019-05-17 13:36:02 +0200

    dataformatter

--- app/src/main/java/dk/cs/aau/envue/shared/Dataformatter.kt
+++ app/src/main/java/dk/cs/aau/envue/shared/Dataformatter.kt
@@ -1,2 +1,9 @@
 package dk.cs.aau.envue.shared
 
+import java.text.SimpleDateFormat
+import java.util.*
+
+fun FormatDate(date:String): Date {
+    val dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+    return dateFormat.parse(date)
+}
