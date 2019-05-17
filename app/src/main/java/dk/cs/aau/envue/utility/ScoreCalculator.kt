0ef3b07fddfc1f99bdca21484d8670b07f63173c
Theis Jendal <jendal@live.dk>
2019-05-17 22:50:20 +0200

    Converts miliseconds to seconds now

--- app/src/main/java/dk/cs/aau/envue/utility/ScoreCalculator.kt
+++ app/src/main/java/dk/cs/aau/envue/utility/ScoreCalculator.kt
@@ -3,9 +3,11 @@ import java.util.*
 
 fun calculateScoreFromViewTime(joinedTimeStamp: MutableList<Pair<String, Int>>, leftTimeStamp: MutableList<Pair<String, Int>>, date: Date): Int {
     var score = 0
+    val lastActivity = date.time / 1000
+
     joinedTimeStamp.forEach {
         val pair = leftTimeStamp.find { x -> x.first == it.first}
-        val time = pair?.second?.toLong() ?: date.time
+        val time = pair?.second?.toLong() ?: lastActivity
         val difference = (time - it.second.toLong())
 
         if (difference >= 30) score += difference.toInt()  // Only add viewing for minimum of 30 seconds
