b98a0c08cd53eaaad1c65379410441ff678d0c2e
Anders Jakobsen <andersjkbsn@gmail.com>
2019-05-18 21:54:00 +0200

    Renamed fragment

--- app/src/main/java/dk/cs/aau/envue/PlayerActivity.kt
+++ app/src/main/java/dk/cs/aau/envue/PlayerActivity.kt
@@ -6,8 +6,6 @@ import android.content.res.Configuration
 import android.net.Uri
 import android.os.AsyncTask
 import android.os.Bundle
-import android.speech.tts.TextToSpeech
-import android.support.v4.app.Fragment
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
