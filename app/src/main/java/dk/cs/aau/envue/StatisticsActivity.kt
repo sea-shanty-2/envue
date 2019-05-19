d4b9a2e2c2f5f2e6fb7b619369da01353c4d4488
Anders Jakobsen <andersjkbsn@gmail.com>
2019-05-19 13:57:35 +0200

    Updated statistics activity

--- app/src/main/java/dk/cs/aau/envue/StatisticsActivity.kt
+++ app/src/main/java/dk/cs/aau/envue/StatisticsActivity.kt
@@ -1,15 +1,20 @@
 package dk.cs.aau.envue
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
+import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
 
 class StatisticsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_statistics)
 
         if (savedInstanceState == null) {
+            // Get score
+            val score = intent.extras?.getInt("score")
+            score?.let { findViewById<TextView>(R.id.score_increased)?.text = getString(R.string.score_increased, it) }
 
             // Pass on viewership data to the ViewershipFragment
             val viewership = ViewershipFragment()
@@ -20,6 +25,8 @@ class StatisticsActivity : AppCompatActivity() {
                 .commit()
         }
 
-        setContentView(R.layout.activity_statistics)
+        findViewById<FloatingActionButton>(R.id.close_statistics_button).setOnClickListener {
+            onBackPressed()  // TODO: Do something more like calculate score first :-)
+        }
     }
 }
